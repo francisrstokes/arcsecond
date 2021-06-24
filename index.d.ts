1367c0ab57d7e950a6e8ba0d49a39a118293a64c
Frank Stokes <frank@wearereasonablepeople.com>
2021-06-24 21:32:34 +0200

    Add generic typing to the primary result where possible

--- index.d.ts
+++ index.d.ts
@@ -23,14 +23,14 @@ type TargetType =
   | Float32Array
   | Float64Array;
 
-export type ParserState = {
+export type ParserState<T> = {
   dataView: DataView;
   inputType: InputType;
   isError: boolean;
   error: any;
   data: any;
   index: number;
-  result: any;
+  result: T;
 }
 
 export type ResultType<T> = {
@@ -40,72 +40,72 @@ export type ResultType<T> = {
   result: T;
 }
 
-export type StateTransformerFunction = (state: ParserState) => ParserState;
+export type StateTransformerFunction<T> = (state: ParserState<any>) => ParserState<T>;
 
-declare class Parser {
-  constructor(p: StateTransformerFunction);
-  run<T>(target: TargetType): ResultType<T>;
-  fork<T>(target: TargetType, errorFn: (err: any) => T, successFn: (result: any) => T): T;
-  map(fn: (x: any) => any): Parser;
-  chain(fn: (x: any) => Parser): Parser;
-  ap(parserOfFunction: Parser): Parser;
-  errorMap(fn: (err: any) => any): Parser;
-  errorChain(fn: (err: any) => Parser): Parser;
-  mapFromData(fn: (data: any) => any): Parser;
-  chainFromData(fn: (data: any) => Parser): Parser;
-  mapData(fn: (data: any) => any): Parser;
-  of(x: any): Parser;
+declare class Parser<T> {
+  constructor(p: StateTransformerFunction<T>);
+  run(target: TargetType): ResultType<T>;
+  fork(target: TargetType, errorFn: (err: any) => T, successFn: (result: any) => T): T;
+  map<T2>(fn: (x: T) => T2): Parser<T2>;
+  chain<T2>(fn: (x: T) => Parser<T2>): Parser<T2>;
+  ap<T2>(parserOfFunction: Parser<(x: T) => T2>): Parser<T2>;
+  errorMap(fn: (err: any) => any): Parser<T>;
+  errorChain<T2>(fn: (err: any) => Parser<T2>): Parser<T2>;
+  mapFromData(fn: (data: any) => any): Parser<T>;
+  chainFromData<T2>(fn: (data: any) => Parser<T2>): Parser<T2>;
+  mapData(fn: (data: any) => any): Parser<T>;
+  static of<T>(x: T): Parser<T>;
 }
 
-export type FnReturingParserIterator = () => Iterator<Parser, any>;
+export type FnReturingParserIterator<T> = () => Iterator<Parser<any>, T>;
 
-declare const getData: Parser;
-declare function setData(data: any): Parser;
-declare function mapData(mapFn: (data: any) => any): Parser;
-declare function withData(parser: Parser): (data: any) => Parser;
-declare function pipeParsers(parsers: Parser[]): Parser;
-declare function composeParsers(parsers: Parser[]): Parser;
-declare function tapParser(fn: (state: ParserState) => void): Parser;
-declare function parse<T>(parser: Parser): (target: TargetType) => ResultType<T>;
-declare function decide(fn: (state: ParserState) => Parser): Parser;
-declare function fail(errorValue: any): Parser;
-declare function succeedWith(value: any): Parser;
-declare function either(parser: Parser): Parser;
-declare function coroutine(fn: FnReturingParserIterator): Parser;
-declare function exactly(n: Number): (p: Parser) => Parser;
-declare function many(p: Parser): Parser;
-declare function many1(p: Parser): Parser;
-declare function mapTo(fn: (x: any) => any): Parser;
-declare function errorMapTo(fn: (err: any) => any): Parser;
-declare function char(c: string): Parser;
-declare const anyChar: Parser;
-declare const peek: Parser;
-declare function str(s: string): Parser;
-declare function regex(re: RegExp): Parser;
-declare const digit: Parser;
-declare const digits: Parser;
-declare const letter: Parser;
-declare const letters: Parser;
-declare function anyOfString(s: string): Parser;
-declare function namedSequenceOf(namedParserPairs: Array<[string, Parser]>): Parser;
-declare function sequenceOf(parsers: Array<Parser>): Parser;
-declare function sepBy(sepParser: Parser): (valueParser: Parser) => Parser;
-declare function sepBy1(sepParser: Parser): (valueParser: Parser) => Parser;
-declare function choice(parsers: Array<Parser>): Parser;
-declare function between(left: Parser): (right: Parser) => (parser: Parser) => Parser;
-declare function everythingUntil(parser: Parser): Parser;
-declare function everyCharUntil(parser: Parser): Parser;
-declare function anythingExcept(parser: Parser): Parser;
-declare function anyCharExcept(parser: Parser): Parser;
-declare function lookahead(parser: Parser): Parser;
-declare function possibly(parser: Parser): Parser;
-declare function skip(parser: Parser): Parser;
-declare const startOfInput: Parser;
-declare const endOfInput: Parser;
-declare const whitespace: Parser;
-declare const optionalWhitespace: Parser;
-declare function recursiveParser(parserThunk: () => Parser): Parser;
-declare function takeRight(left: Parser): (right: Parser) => Parser;
-declare function takeLeft(left: Parser): (right: Parser) => Parser;
+declare const getData: Parser<any>;
+declare function setData(data: any): Parser<any>;
+declare function mapData(mapFn: (data: any) => any): Parser<any>;
+declare function withData<T>(parser: Parser<T>): (data: any) => Parser<T>;
+declare function pipeParsers(parsers: Parser<any>[]): Parser<any>;
+declare function composeParsers(parsers: Parser<any>[]): Parser<any>;
+declare function tapParser(fn: (state: ParserState<any>) => void): Parser<any>;
+declare function parse<T>(parser: Parser<T>): (target: TargetType) => ResultType<T>;
+declare function decide<T>(fn: (state: ParserState<any>) => Parser<T>): Parser<T>;
+declare function fail(errorValue: any): Parser<any>;
+declare function succeedWith<T>(value: T): Parser<T>;
+declare function either<T>(parser: Parser<T>): Parser<T>;
+declare function coroutine<T>(fn: FnReturingParserIterator<T>): Parser<T>;
+declare function exactly<T>(n: Number): (p: Parser<T>) => Parser<T[]>;
+declare function many<T>(p: Parser<T>): Parser<T[]>;
+declare function many1<T>(p: Parser<T>): Parser<T[]>;
+declare function mapTo<T>(fn: (x: any) => T): Parser<T>;
+declare function errorMapTo(fn: (err: any) => any): Parser<any>;
+declare function char(c: string): Parser<string>;
+declare const anyChar: Parser<string>;
+declare const peek: Parser<number>;
+declare function str(s: string): Parser<string>;
+declare function regex(re: RegExp): Parser<string>;
+declare const digit: Parser<string>;
+declare const digits: Parser<string>;
+declare const letter: Parser<string>;
+declare const letters: Parser<string>;
+declare function anyOfString(s: string): Parser<string>;
+declare function namedSequenceOf(namedParserPairs: Array<[string, Parser<any>]>): Parser<any[]>;
+declare function sequenceOf(parsers: Array<Parser<any>>): Parser<any[]>;
+declare function sepBy<S, T>(sepParser: Parser<S>): (valueParser: Parser<T>) => Parser<T[]>;
+declare function sepBy1<S, T>(sepParser: Parser<S>): (valueParser: Parser<T>) => Parser<T[]>;
+declare function choice(parsers: Array<Parser<any>>): Parser<any>;
+declare function between<L, T, R>(left: Parser<L>): (right: Parser<R>) => (parser: Parser<T>) => Parser<T>;
+declare function everythingUntil(parser: Parser<any>): Parser<number[]>;
+declare function everyCharUntil(parser: Parser<any>): Parser<string>;
+declare function anythingExcept(parser: Parser<any>): Parser<number>;
+declare function anyCharExcept(parser: Parser<any>): Parser<number>;
+declare function lookahead<T>(parser: Parser<T>): Parser<T>;
+declare function possibly<T>(parser: Parser<T>): Parser<T | null>;
+declare function skip(parser: Parser<any>): Parser<null>;
+declare const startOfInput: Parser<null>;
+declare const endOfInput: Parser<null>;
+declare const whitespace: Parser<string>;
+declare const optionalWhitespace: Parser<string>;
+declare function recursiveParser<T>(parserThunk: () => Parser<T>): Parser<T>;
+declare function takeRight<L, R>(left: Parser<L>): (right: Parser<R>) => Parser<R>;
+declare function takeLeft<L, R>(left: Parser<L>): (right: Parser<R>) => Parser<L>;
 declare function toPromise<T>(result: ResultType<T>): Promise<T>;
 declare function toValue<T>(result: ResultType<T>): T;
