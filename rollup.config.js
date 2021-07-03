25353e500b60ec615b93b4f6d8444e462c9aedc4
Nitwel <nitwel@arcor.de>
2021-07-03 20:01:23 +0200

    use rollup to build and add multiple versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,29 @@
-import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: './dist/index.js',
-  output: {
-      file: 'bundle.js',
-      format: 'cjs'
+export default [
+  {
+    input: './src/index.ts',
+    output: {
+        file: 'index.js',
+        format: 'cjs'
+    },
+    plugins: [typescript()]
   },
-  plugins: [commonjs()]
-};
+  {
+    input: './src/index.ts',
+    output: {
+        file: 'index.mjs',
+        format: 'es'
+    },
+    plugins: [typescript()]
+  },
+  {
+    input: './src/index.ts',
+    output: {
+        file: 'index.d.ts',
+        format: 'cjs'
+    },
+    plugins: [typescript(), dts()]
+  },
+]
